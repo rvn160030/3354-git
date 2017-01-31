7d080d75a6bb003b6c02e4f403e02b724fdbc704
Richard Noble <rvn160030@utdallas.edu>
2017-01-31 15:47:23 -0600

    Updated Main.java Functionality was added to Main.java to check for incorrect parameters. Added new exception handling and output to user.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -4,17 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+            if(args.length == 0){ throw new IllegalArgumentException();}
             int result = addArguments(args);
             System.out.println(result);
-        } catch (Exception e) {
-            System.err.println("Please provide some integers to add");
-        }
+        } catch (IllegalArgumentException e) {
+            if(args.length == 0){
+		System.err.println("Please provide at least 2 integers");
+	    }
+	    else{
+	    System.err.println("Please provide only integer arguments");
+    	    }
+	}
     }
 
     private static int addArguments(String[] args) {
-        int output = 0;
+	int output = 0;
 	for(int i = 0; i < args.length; i++){
-		output+= Intrger.valueOf(args[i]);
+		output+= Integer.valueOf(args[i]);
 	}
 	return output;
     }
