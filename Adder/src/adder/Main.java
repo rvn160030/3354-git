26f4a89bf8790ee5822f6155852df8efb918a622
Richard Noble <rvn160030@utdallas.edu>
2017-01-31 14:43:50 -0600

    Updated Main.java Added functionality to Main to include adding negative numbers. Changed Main.java to recognize new argument "-".

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,14 +7,24 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide some integers to add");
+            System.err.println("Please provide some integers to add, use \"-\""
+		+ " to add as negative integers");
         }
     }
 
     private static int addArguments(String[] args) {
         int output = 0;
-	for(int i = 0; i < args.length; i++){
-		output+= Intrger.valueOf(args[i]);
+	if(args[0].equals("-")){
+		//System.out.println("Negative addition");
+		for(int i = 1; i < args.length; i++){
+			output-= Integer.valueOf(args[i]);
+		}
+	}
+	else {
+		for(int i = 0; i < args.length; i++){
+			//System.out.println("Positive Addition");
+			output+= Integer.valueOf(args[i]);
+		}
 	}
 	return output;
     }
